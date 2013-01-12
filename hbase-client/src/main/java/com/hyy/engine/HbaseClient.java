9f2906962b654479bab031ad564baf9d9b08bcd6
Lv Henry <henry.lv206@gmail.com>
2013-01-13 00:25:57 +0800

    add hbase client code

--- hbase-client/src/main/java/com/hyy/engine/HbaseClient.java
+++ hbase-client/src/main/java/com/hyy/engine/HbaseClient.java
@@ -17,7 +17,7 @@ public class HbaseClient {
 	}
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @thros IOException 
 	 */
 	public static void main(String[] args) throws IOException {
 		HbaseClient hClient = new HbaseClient();
@@ -47,5 +47,84 @@ public class HbaseClient {
 			admin.createTable(tableDesc);
 			System.out.println("The table created succeed.");
 		}
+	}
+	
+	/**
+	 * delete table
+	 * @param tableName
+	 * @throws IOException
+	 */
+	public void deleteTable(String tableName) throws IOException {		
+		HBaseAdmin admin = new HBaseAdmin(conf);
+		admin.disableTable(tableName);
+		admin.deleteTable(tableName);
+		System.out.println("表删除成功.");
+	}
+	
+	/**
+	 * insert
+	 * @param tableName
+	 * @param cfs
+	 */
+	public void writeRow(String tableName, String[] cfs) {
+		HTable table = new HTable(conf, tableName);
+		Put put = new Put(Bytes.toBytes("rows1"));
+		
+		for (String cf : cfs) {
+			put.add(Bytes.toBytes(cf), Bytes.toBytes(String.valueOf(1)), Bytes.toBytes("value_1"));
+			table.put(put);
+		}
+	}
+	
+	/**
+	 * delete
+	 * @param tableName
+	 * @param rowKey
+	 */
+	public void deleteRow(String tableName, String rowKey) {
+		HTable table = new HTable(conf, tablename);
+	    List list = new ArrayList();
+	    Delete d1 = new Delete(rowkey.getBytes());
+	    list.add(d1);
+	    table.delete(list);
+	    System.out.println("删除行成功！");
+	}
+	
+	/**
+	 * select 
+	 * @param tableName
+	 * @param rowKey
+	 */
+	public void selectRow(String tableName, String rowKey) {
+		HTable table = new HTable(conf, tablename);
+	    Get g = new Get(rowKey.getBytes());
+	    Result rs = table.get(g);
+	    for (KeyValue kv : rs.raw()) {
+	        System.out.print(new String(kv.getRow()) + "  ");
+	        System.out.print(new String(kv.getFamily()) + ":");
+	        System.out.print(new String(kv.getQualifier()) + "  ");
+	        System.out.print(kv.getTimestamp() + "  ");
+	        System.out.println(new String(kv.getValue()));
+	    }
+	}
+	
+	/**
+	 * list all data
+	 * @param tableName
+	 */
+	public void scanner(String tableName) {
+		HTable table = new HTable(conf, tablename);
+        Scan s = new Scan();
+        ResultScanner rs = table.getScanner(s);
+        for (Result r : rs) {
+            KeyValue[] kv = r.raw();
+            for (int i = 0; i < kv.length; i++) {
+                System.out.print(new String(kv[i].getRow()) + "  ");
+                System.out.print(new String(kv[i].getFamily()) + ":");
+                System.out.print(new String(kv[i].getQualifier()) + "  ");
+                System.out.print(kv[i].getTimestamp() + "  ");
+                System.out.println(new String(kv[i].getValue()));
+            }
+        }
 	}
 }
