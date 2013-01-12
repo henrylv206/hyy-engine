c896dc8a3f11dd283bb3730dd433887e9d01bbfb
Lv Henry <henry.lv206@gmail.com>
2013-01-13 01:27:33 +0800

    finish Hbase client code

--- hbase-client/src/main/java/com/hyy/engine/HbaseClient.java
+++ hbase-client/src/main/java/com/hyy/engine/HbaseClient.java
@@ -1,12 +1,23 @@
 package com.hyy.engine;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
 
 public class HbaseClient {
 
@@ -65,8 +76,9 @@ public class HbaseClient {
 	 * insert
 	 * @param tableName
 	 * @param cfs
+	 * @throws IOException 
 	 */
-	public void writeRow(String tableName, String[] cfs) {
+	public void writeRow(String tableName, String[] cfs) throws IOException {
 		HTable table = new HTable(conf, tableName);
 		Put put = new Put(Bytes.toBytes("rows1"));
 		
@@ -80,11 +92,12 @@ public class HbaseClient {
 	 * delete
 	 * @param tableName
 	 * @param rowKey
+	 * @throws IOException 
 	 */
-	public void deleteRow(String tableName, String rowKey) {
-		HTable table = new HTable(conf, tablename);
+	public void deleteRow(String tableName, String rowKey) throws IOException {
+		HTable table = new HTable(conf, tableName);
 	    List list = new ArrayList();
-	    Delete d1 = new Delete(rowkey.getBytes());
+	    Delete d1 = new Delete(rowKey.getBytes());
 	    list.add(d1);
 	    table.delete(list);
 	    System.out.println("É¾³ýÐÐ³É¹¦£¡");
@@ -94,11 +107,12 @@ public class HbaseClient {
 	 * select 
 	 * @param tableName
 	 * @param rowKey
+	 * @throws IOException 
 	 */
-	public void selectRow(String tableName, String rowKey) {
-		HTable table = new HTable(conf, tablename);
-	    Get g = new Get(rowKey.getBytes());
-	    Result rs = table.get(g);
+	public void selectRow(String tableName, String rowKey) throws IOException {
+		HTable table = new HTable(conf, tableName);
+	    Get get = new Get(rowKey.getBytes());
+	    Result rs = table.get(get);
 	    for (KeyValue kv : rs.raw()) {
 	        System.out.print(new String(kv.getRow()) + "  ");
 	        System.out.print(new String(kv.getFamily()) + ":");
@@ -111,9 +125,10 @@ public class HbaseClient {
 	/**
 	 * list all data
 	 * @param tableName
+	 * @throws IOException 
 	 */
-	public void scanner(String tableName) {
-		HTable table = new HTable(conf, tablename);
+	public void scanner(String tableName) throws IOException {
+		HTable table = new HTable(conf, tableName);
         Scan s = new Scan();
         ResultScanner rs = table.getScanner(s);
         for (Result r : rs) {
