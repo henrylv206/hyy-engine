6d34951398125926a1f0192e9d13f240a1e58dbe
HenryLv <lvhonglei@rockontrol.com>
2014-01-08 13:10:02 +0800

    add hyy-utils project

---
+++ hyy-utils/src/main/java/com/hyy/engine/utils/FileUtils.java
@@ -0,0 +1,46 @@
+package com.hyy.engine.utils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 1. 解析html文件中的id和class
+ * 2. 解析css文件中的样式
+ * 3. 提取css文件中需要的样式
+ * 
+ * @author Henry
+ *
+ * @since 2014-1-8 12:58
+ * @version 0.1
+ */
+public class FileUtils {
+	
+	/**
+	 * 解析html文件中的id和class
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public static List<String> parseHTML(String htmlPath) {
+		return null;
+	}
+	
+	/**
+	 * 根据id或class获取css文件中的样式
+	 * 
+	 * @param list id或class
+	 * @return id或class: 样式
+	 */
+	public static Map<String, String> parseCSS(String cssPath, List<String> list) {
+		return null;
+	}
+	
+	/**
+	 * 解析html和css文件，生成需要的css文件
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
